1895e5b447df6b3684d4a5d7a9e512afb813b9de
VR Jammer <vrjammer@subtlemail.com>
2017-11-07 00:38:29 -0600

    Update version of the terminal hacker game + webgl build

--- 2_terminal_hacker/Terminal Hacker/Assets/Hacker.cs
+++ 2_terminal_hacker/Terminal Hacker/Assets/Hacker.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
@@ -12,8 +9,14 @@ public class Hacker : MonoBehaviour {
 
     enum Screen { MainMenu, GuessPassword, WinScreen };
 
+    const string menuHint = "You may type menu at any time";
+    string[] level1Passwords = { "books", "aisle", "shelf", "password", "font", "borrow" };
+    string[] level2Passwords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
+    string[] level3Passwords = { "starfield", "telescope", "environment", "exploration", "astronauts" };
+
     Screen currentScreen = Screen.MainMenu;
     int level;
+    string password;
 
 	void ShowMainMenu() {
         currentScreen = Screen.MainMenu;
@@ -39,23 +42,31 @@ public class Hacker : MonoBehaviour {
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.GuessPassword)
+        {
+            RunGuessPassword(input);
+        }
+    }
+
+    void RunGuessPassword(string input)
+    {
+        if (input == password)
+        {
+            WinScreen();
+        }
+        else
+        {
+            GameStart();
+        }
     }
 
     private void RunMainMenu(string input)
     {
-        if (input == "1")
+        bool isValidLevelNumber =
+            (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
         {
-            level = 1;
-            GameStart();
-        }
-        else if (input == "2")
-        {
-            level = 2;
-            GameStart();
-        }
-        else if (input == "3")
-        {
-            level = 3;
+            level = int.Parse(input);
             GameStart();
         }
         else if (input == "42")
@@ -71,7 +82,79 @@ public class Hacker : MonoBehaviour {
     void GameStart()
     {
         currentScreen = Screen.GuessPassword;
-        Terminal.WriteLine("Please enter your password:");
+        ChooseLevelPassword();
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Enter the password, hint: "+ password.Anagram());
+    }
+
+    void ChooseLevelPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+    }
+
+    void WinScreen()
+    {
+        currentScreen = Screen.WinScreen;
+        Terminal.ClearScreen();
+        LevelSpecificScreen();
+    }
+
+    void LevelSpecificScreen()
+    {
+        Terminal.WriteLine("Let's play !");
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine(@"
+      / X / O
+  ___/___/___
+  O /   / 
+___/___/___
+  /   /
+ /   /  X
+");
+                break;
+            case 2:
+                Terminal.WriteLine(@"
+  __/'''\
+ ]___ 0  }
+    /    }
+   /~    }
+   \____/
+   /____\
+  (______)
+");
+                break;
+            case 3:
+                Terminal.WriteLine(@"
+   ..-^~~~^-..
+ .~           ~.
+(;:           :;)
+  ':._     _.:'
+       | |
+     (=====)
+    ((/   \))
+");
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
+        Terminal.WriteLine(menuHint);
     }
 
     // Update is called once per frame
